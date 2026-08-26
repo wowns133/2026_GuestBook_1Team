cab3c2b650aa11ce91dd18d4045f6163d8d52d8c
202307018 <challenjoy@naver.com>
2026-08-26 18:25:14 +0900

    그리기 방식을 GDI+를 이용한 더블 버퍼링 방식으로 수정하였습니다. 수정 내용은 아래와 같습니다. Draw.h, Draw.cpp를 수정하였습니다. Draw 클래스 내부 항목과 일부 외부 항목이 변경되었습니다. Draw 클래스 내부의 startDrawingLine, drawingLine, EndDrawingLine 함수의 그리기 방식이 GDI+를 이용한 더블 버퍼링 방식으로 변경되었습니다. 이에 따라 GDI+를 시작하고 끝내는 gdiPlusStart, gdiPlusEnd 함수가 추가되었습니다. GDI+에 이용될 멤버 변수들이 추가되었습니다. 상세는 주석을 참고해 주시면 감사하겠습니다. GDI+ 적용을 위한 include문과 pragma문이 추가되었습니다. framework.h의#define WIN32_LEAN_AND_MEAN 로 인해 일부 헤더가 제외되어 gdiplus.h에서 오류가 발생하는 일을 방지하기 위해 unKnwn.h가 추가되었습니다.
    각종 버그와 개선점에 관해서는 Draw.h의 파일 주석에 포함된 TODO를 참고
    바랍니다.

--- 2026_GuestBook_1Team/2026_GuestBook_1Team/Draw.cpp
+++ 2026_GuestBook_1Team/2026_GuestBook_1Team/Draw.cpp
@@ -1,15 +1,124 @@
-#pragma once
 #include "Draw.h"
+
 /**
 * @file
 * @brief 그리기 기능을 구현하는 파일.
 * @details 그리기 기능을 구현한 파일. Draw.h의 함수들이 구현되어 있다.
+* @author challenjoy01
+* @warning startDrawingLine 내부에는 객체 생성 등 자원 해제가 필요한 부분이 존재합니다. startDrawingLine 실행 후에는 
+* 반드시 EndDrawingLine이 실행될 수 있도록 주의해야 합니다.
 */
+
+
+void Draw::drawWindowLines(HWND hWnd, HDC hdc)
+{
+	for (size_t line_num = 0; line_num < drawn_lines.size(); line_num++)
+	{
+		for (size_t point_num = 1; point_num < drawn_lines[line_num].size(); point_num++)
+		{
+			MoveToEx(hdc, drawn_lines[line_num][point_num - 1].x, drawn_lines[line_num][point_num - 1].y, NULL);
+			LineTo(hdc, drawn_lines[line_num][point_num].x, drawn_lines[line_num][point_num].y);
+		}
+	}
+
+	for (size_t point_num = 1; point_num < drawn_line.size(); point_num++)
+	{
+		MoveToEx(hdc, drawn_line[point_num - 1].x, drawn_line[point_num - 1].y, NULL);
+		LineTo(hdc, drawn_line[point_num].x, drawn_line[point_num].y);
+	}
+}
+
+void Draw::gdiPlusStart()
+{
+	Gdiplus::GdiplusStartupInput gdi_plus_start_up_input;
+	Gdiplus::GdiplusStartup(&drawing_token, &gdi_plus_start_up_input, NULL);
+}
+
+void Draw::gdiPlusEnd()
+{
+	Gdiplus::GdiplusShutdown(drawing_token);
+}
+
 
 void Draw::startDrawingLine(HWND hWnd, LPARAM lParam)
 {
+	//그리기 환경 제작
+	draw_hdc = GetDC(hWnd); //hdc 발행. 사용 후 회수해야 함
+	draw_hdc_graphics = new Gdiplus::Graphics(draw_hdc); //화면 출력을 담당할 Graphics 객체 생성. 사용 후 삭제해야 함
+	GetClientRect(hWnd, &client_rect); //client_rect에 현재 작업영역 크기 구하기
+	draw_bmp = new Gdiplus::Bitmap(client_rect.right, client_rect.bottom, PixelFormat32bppARGB); //더블 버퍼링 구현을 위한 Bitmap 객체 생성. 사용 후 삭제해야 함
+	draw_bmp_graphics = new Gdiplus::Graphics(draw_bmp); //비트맵 출력을 담당할 Graphics 객체 생성. 사용 후 삭제해야 함
+
 	//선을 그리기 시작하는 좌표를 현재 좌표로 초기화
 	previous_x = LOWORD(lParam); //lParam의 하위 16비트를 가져와 x좌표로 저장 
+	previous_y = HIWORD(lParam); //lParam의 상위 16비트를 가져와 y좌표로 저장
+
+	//현재 좌표를 선의 시작점으로 저장
+	drawn_line.push_back({ previous_x, previous_y });
+
+	//그리는 중이라고 플래그 표시
+	is_drawing = true;
+}
+
+
+void Draw::drawingLine(HWND hWnd, LPARAM lParam)
+{
+	if(is_drawing)
+	{
+	/*------------------임시 펜 조작-----------------------*/
+	Gdiplus::Pen pen(Gdiplus::Color(255, 0, 0, 0), 1.0f); 
+	pen.SetStartCap(Gdiplus::LineCapRound);
+	pen.SetEndCap(Gdiplus::LineCapRound);
+
+	//이동 후 현재 위치를 저장
+	current_x = LOWORD(lParam);
+	current_y = HIWORD(lParam);
+
+	//현재 위치 좌표를 벡터에 저장
+	drawn_line.push_back({ current_x, current_y });
+
+	/*--------------비트맵에 그리기--------------*/
+	draw_bmp_graphics->SetSmoothingMode(Gdiplus::SmoothingModeAntiAlias);				// 안티엘리어싱 적용. 선에서 계단 현상이 줄어든다.
+	draw_bmp_graphics->DrawLine(&pen, previous_x, previous_y, current_x, current_y);	// 선 긋기 함수 실행
+
+	previous_x = current_x; //현재 위치를 이동 전 좌표 변수에 저장
+	previous_y = current_y; //현재 위치를 이동 전 좌표 변수에 저장
+
+	draw_hdc_graphics->DrawImage(draw_bmp, 0, 0); // 화면에 미리 그려놓은 비트맵을 출력
+	}
+}
+
+void Draw::endDrawingLine(HWND hWnd, LPARAM lParam)
+{
+	drawn_lines.push_back(drawn_line); //다 그린 선을 선을 모아놓은 벡터에 저장
+	drawn_line.clear(); //선 벡터 비우기
+	is_drawing = false; //그리기 끝났으므로 플래그를 false로 변경
+
+
+	/*--------------------포인터 처리-------------------*/
+	delete draw_bmp_graphics;
+	delete draw_bmp;
+
+	delete draw_hdc_graphics; //화면 출력을 담당할 Graphics 객체 파괴
+	ReleaseDC(hWnd, draw_hdc); //hdc 파괴
+
+	draw_bmp_graphics = nullptr;
+	draw_bmp = nullptr;
+	draw_hdc_graphics = nullptr;
+}
+
+
+
+// 더블 버퍼링을 적용하지 않은 단순 선 그리기 함수들
+// void startDrawingLine(HWND hWnd, LPARAM lParam);
+// void drawingLine(HWND hWnd, LPARAM lParam);
+// void endDrawingLine(HWND hWnd, LPARAM lParam);
+// 위 세 함수의 이전 버전입니다.
+/*
+void Draw::startDrawingLine(HWND hWnd, LPARAM lParam)
+{
+	//선을 그리기 시작하는 좌표를 현재 좌표로 초기화
+	previous_x = LOWORD(lParam); //lParam의 하위 16비트를 가져와 x좌표로 저장
 	previous_y = HIWORD(lParam); //lParam의 상위 16비트를 가져와 y좌표로 저장
 
 	//현재 좌표를 선의 시작점으로 저장
@@ -48,21 +157,5 @@ void Draw::endDrawingLine(HWND hWnd, LPARAM lParam)
 	drawn_line.clear(); //선 벡터 비우기
 	is_drawing = false; //그리기 끝났으므로 플래그를 false로 변경
 }
+*/
 
-void Draw::drawWindowLines(HWND hWnd, HDC hdc)
-{
-	for (size_t line_num = 0; line_num < drawn_lines.size(); line_num++)
-	{
-		for (size_t point_num = 1; point_num < drawn_lines[line_num].size(); point_num++)
-		{
-			MoveToEx(hdc, drawn_lines[line_num][point_num - 1].x, drawn_lines[line_num][point_num - 1].y, NULL);
-			LineTo(hdc, drawn_lines[line_num][point_num].x, drawn_lines[line_num][point_num].y);
-		}
-	}
-
-	for (size_t point_num = 1; point_num < drawn_line.size(); point_num++)
-	{
-		MoveToEx(hdc, drawn_line[point_num - 1].x, drawn_line[point_num - 1].y, NULL);
-		LineTo(hdc, drawn_line[point_num].x, drawn_line[point_num].y);
-	}
-}
--- 2026_GuestBook_1Team/2026_GuestBook_1Team/Draw.h
+++ 2026_GuestBook_1Team/2026_GuestBook_1Team/Draw.h
@@ -1,18 +1,32 @@
 #pragma once
-#include <vector>
+
 #include <Windows.h>
+#include <unknwn.h>        // GdiplusGraphics.h 내 IUnknown 빨간줄 해결 핵심
+#include <gdiplus.h>
+#pragma comment(lib, "gdiplus.lib")
+#include <vector>
+
+
 /**
 * @file
 * @brief 그리기 기능을 구현하는 헤더 파일.
 * @details 그리기 기능을 구현한 헤더 파일.
 * @todo 1. 화면 안에서 클릭한 후 화면 밖에서 땠을 때 다시 화면 안으로 돌아오면 화면 밖에서 생긴 이벤트를 감지할 수 없는 문제 해결
 * 2. 그릴 때 시간 측정하는 기능 추가
+* 3. drawingLine에는 임시 펜이 적용되어 있음. 추후 수정
+* 4. 빠르게 그렸을 때 선이 각지게 그려지는 문제 해결
+* 5. drawWindowLines 함수 더블 버퍼링 버전으로 변경
+* @warning startDrawingLine 내부에는 객체 생성 등 자원 해제가 필요한 부분이 존재합니다. startDrawingLine 실행 후에는 
+* 반드시 EndDrawingLine이 실행될 수 있도록 주의해야 합니다.
+* @author challenjoy01
 */
+
 
 
 /**
 * @brief 그리기 기능을 담당하는 클래스.
 * @details 그리기 기능을 담당하는 클래스.
+* @author challenjoy01
 */
 class Draw
 {
@@ -26,37 +40,96 @@ private:
 
 	std::vector<std::vector<POINT>> drawn_lines; ///< 선들의 집합을 저장하는 vector. 즉 모든 선을 저장하는 vector
 	std::vector<POINT> drawn_line;///< 그려진 점들의 집합을 저장하는 vector. 즉 하나의 선을 저장하는 vector
+
+	//------------------------GDI+---------------------//
+	ULONG_PTR drawing_token; ///< GDI 토큰 핸들
+
+	HDC draw_hdc; ///< Draw 클래스 멤버 변수로 선언된 HDC
+	Gdiplus::Graphics* draw_hdc_graphics = nullptr; ///< Draw 클래스 멤버 포인터 변수로 선언된 Graphics. hdc를 삭제하기 전 반드시 파괴해야 한다.
+	Gdiplus::Bitmap* draw_bmp = nullptr; ///< Draw 클래스 멤버 포인터 변수로 선언된 Bitmap
+	Gdiplus::Graphics* draw_bmp_graphics = nullptr; ///< Draw 클래스 멤버 포인터 변수로 선언된 Graphics. Bitmap를 삭제하기 전 반드시 파괴해야 한다.
+
+	RECT client_rect; ///< 작업 영역의 크기를 저장하기 위한 RECT. right가 너미, bottom이 높이.
+
+
 protected:
 
 public:
 	/**
-	* @brief 선 그리기 시작 함수
+	* @brief 더블 버퍼링이 적용된 선 그리기 시작 함수. 선 그리기에 필요한 각종 값 설정 및 객체 생성을 맡는다.
 	* @details 선 그리기를 시작할 때 시작하는 시점에 실행되는 함수. 마우스 관련 메시지 식별자에서 사용하는 함수이다.
-	* @param[in] hWnd 창 고유 ID
+	* 핵심 동작은 아래와 같다.
+	* 1. 비트맵 생성
+	* 2. 비트맵에 그리기를 담당할 Graphics 객체 생성
+	* 3. hdc 생성
+	* 4. hdc에 그리기를 담당할 Graphics 객체 생성
+	* 5. 이전 좌표 변수에 현재 좌표를 저장
+	* 6. 현재 좌표를 새로운 선 vector에 저장
+	* 7. 그리는 중이라고 플래그 표시
+	* @param[in] hWnd 창 고유 핸들
 	* @param[in] lParam x, y 좌표가 담긴 변수(마우스 메시지가 아닐 경우 lParam 값이 다를 수 있음)
+	* @warning startDrawingLine 내부에는 객체 생성 등 자원 해제가 필요한 부분이 존재합니다. startDrawingLine 실행 후에는 
+	* 반드시 EndDrawingLine이 실행될 수 있도록 주의해야 합니다.
+	* @author challenjoy01
 	*/
 	void startDrawingLine(HWND hWnd, LPARAM lParam);
 	/**
-	* @brief 선 그리기 함수
-	* @details 선을 그리고 그린 선에 대한 좌표값을 저장하는 함수. 마우스 관련 메시지 식별자에서 사용하는 함수이다. 
-	startDrawingLine 함수가 먼저 실행되지 않으면 호출 되어도 동작하지 않고, 시작한 뒤에 endDrawingLine 함수를 실행하지 않으면 호출될 때마다 계속 동작한다.
-	* @param[in] hWnd 창 고유 ID
+	* @brief 더블 버퍼링이 적용된 선 그리기 함수. startDrawingLine 함수가 선행되어야 정상 작동한다. 실행 후 반드시 endDrawingLine 함수를 실행해야 한다.
+	* @details 선을 그리고 그린 선에 대한 좌표값을 저장하는 함수. 그리기 동작에는 더블 버퍼링 기법이 적용되어 있다. 마우스 관련 메시지 식별자에서 사용하는 함수이다.
+	* startDrawingLine 함수가 먼저 실행되지 않으면 호출 되어도 동작하지 않고, 시작한 뒤에 endDrawingLine 함수를 실행하지 않으면 호출될 때마다 계속 동작한다.
+	* 핵심 동작은 아래와 같다.(startDrawingLine으로 인해 플래그가 바뀌었을 경우에만 실행되는 동작이다.)
+	* 1. 펜 생성 및 설정
+	* 2. 현재 위치 변수에 현재 위치를 저장
+	* 3. 현재 위치를 선 벡터에 저장
+	* 4. 비트맵에 선 그리기
+	* 5. 이전 좌표 변수에 현재 좌표를 저장
+	* 6. 비트맵을 화면에 출력
+	* @param[in] hWnd 창 고유 핸들
 	* @param[in] lParam x, y 좌표가 담긴 변수(마우스 메시지가 아닐 경우 lParam 값이 다를 수 있음)
+	* @author challenjoy01
 	*/
 	void drawingLine(HWND hWnd, LPARAM lParam);
 	/**
-	* @brief 선 그리기 종료 함수
-	* @details 선을 그리기를 종료하고, 그린 선을 저장한다. 마우스 관련 메시지 식별자에서 사용하는 함수이다.
-	* @param[in] hWnd 창 고유 ID
+	* @brief 더블 버퍼링이 적용된 선 그리기 종료 함수
+	* @details 선 그리기를 종료하고, 그린 선을 저장하며, 사용이 끝난 객체를 삭제한다. 마우스 관련 메시지 식별자에서 사용하는 함수이다.
+	* 핵심 동작은 아래와 같다.
+	* 1. 그리기를 마친 선을 저장하고 선 벡터의 내용물을 비운다.
+	* 2. 그리기를 완료했다고 플래그를 표시
+	* 3. HDC, 비트맵, 객체 파괴
+	* @param[in] hWnd 창 고유 핸들
 	* @param[in] lParam x, y 좌표가 담긴 변수(마우스 메시지가 아닐 경우 lParam 값이 다를 수 있음)
+	* @author challenjoy01
 	*/
 	void endDrawingLine(HWND hWnd, LPARAM lParam);
 	/**
 	* @brief 화면 전체 선 다시 그리기 함수
-	* @details 화면 전체의 선을 다시 그리는 함수이다. 함수 외부에서 HDC 변수가 먼저 선언되었으며, 
+	* @details 화면 전체의 선을 다시 그리는 함수이다. 함수 외부에서 HDC 변수가 먼저 선언되었으며,
 	함수 실행 후 외부에서 HDC 반환한다는 전제 하에 만들어진 함수이다.
 	* @param[in] hWnd 창 고유 ID
 	* @param[in] hdc 그리기 작업을 수행할 디바이스 컨텍스트(DC) 핸들
+	* @author challenjoy01
 	*/
 	void drawWindowLines(HWND hWnd, HDC hdc);
+
+	//------------------------GDI+ START, END 함수---------------------//
+
+	/**
+	* @brief GDI+ 사용 시작하는 함수
+	* @details GDI+ 사용을 시작하는 함수이다.
+	* GdiplusStartup를 실행한다. 프로그램 시작 시 한 번 사용하고, 마지막 프로그램 종료 직전 GdiplusShutdown 함수를 실행해야 한다.
+	* @author challenjoy01
+	*/
+	void gdiPlusStart();
+
+	/**
+	* @brief GDI+ 사용 종료하는 함수
+	* @details GDI+ 사용을 종료하는 함수이다.
+	* GdiplusShutDown를 실행한다. 프로그램 종료 전 한 번 사용한다.
+	* @author challenjoy01
+	*/
+	void gdiPlusEnd();
+
+	
+
+
 };
